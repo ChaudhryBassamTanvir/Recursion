2abb87c8dbc08397ed03e4fc93fc1fcc0ad65cee
Chaudhry Bassam Tanvir <chbassamtanvir@gmail.com>
2025-09-24 12:25:27 +0500

    Updated

---
+++ RecursionPrintNumb.java
@@ -0,0 +1,24 @@
+public class RecursionPrintNumb {
+
+public static void printNumb(int n){
+
+
+    if (n==6) {
+        return;
+        
+    }
+
+    System.out.print( n+" ");
+    printNumb(n+1);
+
+}
+
+
+    public static void main(String[] args) {
+        
+
+int n=1;
+printNumb(n);
+
+    }
+}
