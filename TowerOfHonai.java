069f400e651a84b2b4cb899534005751cbf10371
Chaudhry Bassam Tanvir <chbassamtanvir@gmail.com>
2025-10-01 13:52:56 +0500

    updated

--- TowerOfHonai.java
+++ TowerOfHonai.java
@@ -1,6 +1,12 @@
 public class TowerOfHonai {
-public static void main(String[] args) {
+
+
+public static void towerHonai(int n, String src, String helper, String dest){
     
+}
+
+public static void main(String[] args) {
+    int n=3;
     
 }
 }
