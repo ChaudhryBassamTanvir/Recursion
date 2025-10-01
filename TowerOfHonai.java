4b046ec2ef13ec66d41fc53c9d54a1cd698b8299
Chaudhry Bassam Tanvir <chbassamtanvir@gmail.com>
2025-10-01 13:58:08 +0500

    updated

--- TowerOfHonai.java
+++ TowerOfHonai.java
@@ -2,11 +2,22 @@ public class TowerOfHonai {
 
 
 public static void towerHonai(int n, String src, String helper, String dest){
-    
+if (n==1) {
+System.out.println("Transfer disk "+ n+ " from" + src +" to" +dest);
+return;
+}
+
+
+towerHonai(n-1, src, dest, helper);
+
+System.out.println("Transfer disk "+ n+ " from" + src +" to" +dest);
+towerHonai(n-1, helper, src, dest);
+
 }
 
 public static void main(String[] args) {
-    int n=3;
-    
+    int n=1;
+
+    towerHonai(n, "S", "H", "D");
 }
 }
