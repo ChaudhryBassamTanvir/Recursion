294c512eaa379f2be86826b475475e18670f9f75
Chaudhry Bassam Tanvir <chbassamtanvir@gmail.com>
2025-10-01 23:08:06 +0500

    Updated

--- FirstandLastOccurance.java
+++ FirstandLastOccurance.java
@@ -1,13 +1,42 @@
 public class FirstandLastOccurance {
 
+         public static int first=-1;
+         public static int second=-1;
+
+    public static void PrintOccurance(String str, int index, char element){
+
+
+        if (index==str.length()) {
+            System.out.println(first);
+            System.err.println(second);
+            return;
+
+            
+        }
+    
+    char currChar = str.charAt(index);
+        if (currChar==element) {
+            if (first==-1) {
+
+                first=index;
+            }else{
+            second=index;
+        }
+
+
+            
+        }
+PrintOccurance(str, index+1, element);
+
+
+    }
 
     public static void main(String[] args) {
 
-String str = "abaacdaefaah";
+  String str = "abaacdaefaah";
 
-        
-        int first=-1;
-        int second=-1;
+
+PrintOccurance(str, 0,'a');
     }
     
 }
