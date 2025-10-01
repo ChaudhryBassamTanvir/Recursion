800bcd2ef1c610f1af7a1d40f30ce5168948aeac
Chaudhry Bassam Tanvir <chbassamtanvir@gmail.com>
2025-10-01 22:50:01 +0500

    Updated

---
+++ FirstandLastOccurance.java
@@ -0,0 +1,13 @@
+public class FirstandLastOccurance {
+
+
+    public static void main(String[] args) {
+
+String str = "abaacdaefaah";
+
+        
+        int first=-1;
+        int second=-1;
+    }
+    
+}
