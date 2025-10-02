de282b1901a88a02cf0ab236a1106f4f08973221
Chaudhry Bassam Tanvir <chbassamtanvir@gmail.com>
2025-10-02 11:55:19 +0500

    updated

--- MoveElement.java
+++ MoveElement.java
@@ -2,10 +2,8 @@ public class MoveElement {
 
 
 
-    public static void moveAllX(String str, String newStr,int index,char x){
- int count=0;
- char currChar= str.charAt(index);
-if (index==str.length()-1) {
+    public static void moveAllX(String str, String newStr,int index,char x,int count){
+        if (index==str.length()) {
  for (int i = 0; i < count; i++) {
   
     newStr +="x";
@@ -14,20 +12,23 @@ if (index==str.length()-1) {
     return;
 }
 
+ char currChar= str.charAt(index);
+
+
 if (currChar== x) {
      count++;
-     moveAllX(str, newStr, index+1, x);
+     moveAllX(str, newStr, index+1, x,count);
      
      
     }else{
     newStr+=currChar;
-      moveAllX(str, newStr, index+1, x);
+      moveAllX(str, newStr, index+1, x,count);
 }
     }
     public static void main(String[] args) {
         String str = "axbcxxd";
 
-moveAllX(str, "",0,'x');
+moveAllX(str, "",0,'x',0);
 
     }
     
